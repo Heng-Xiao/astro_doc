ee32cce135679bf05bc77b0f0a844f0bd5f33c7d
Heng-Xiao <1730509384@qq.com>
2024-08-31 23:54:00 +0800

    新增icp备案信息

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,10 +1,17 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
+import {  FooterCopyright,Section } from 'astro-boilerplate-components';
 
 import { AppConfig } from '@/utils/AppConfig';
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={AppConfig.site_name} />
+      <div style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>
+      <FooterCopyright site_name={AppConfig.site_name} />
+      </div>
+      <div style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="https://beian.miit.gov.cn" style={{ color: '#22d3ee', textDecoration: 'none',fontSize:'14px' }}>
+              黔ICP备2022005951号-1
+          </a>
+      </div>
   </Section>
 );
 
